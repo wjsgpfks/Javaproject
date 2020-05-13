366b9d433cbc4b53cc74b3c06d52060ac7a4c2de
“keiran” <quanhl831@gmail.com>
2020-05-13 12:57:41 +0900

    second commit

--- ReadFileAndWrite.java
+++ ReadFileAndWrite.java
@@ -1,36 +1,84 @@
+//package com.ibm.tsdl.training;//package:In order to better organize the class
+package tsdl.training;
+//ファイルシステムによるシステム入出力用に提供package
+//package for File system input&output
 import java.io.IOException;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner; 
 
+public class ReadFileAndWrite{//Only need one public interface
+    public static void main(String[] args)throws IOException{//if it is finished, it will return -1.This method throws IOException
+        try{
+            Scanner scan = new Scanner(System.in);
+            This_File file = new This_File();//creat object
+            System.out.println("please enter your file path");
+            file.file_path=scan.next();
+            file.ReadFile();
+            List<String> new_list = new ArrayList<String>();
+            new_list = file.ReadFile();
+            System.out.println("Do you want to save new content in new file? Yes or No");
+            Scanner scan_res = new Scanner(System.in);
+            String s = scan_res.next();
+            if(s.equals("Yes") == true){
+                
+                Roleback_File newfile = new Roleback_File();
+                System.out.println("please enter your new file path");
+                Scanner scan_new = new Scanner(System.in);
+                //Roleback_File newfile = new Roleback_File();
+                newfile.newfile_path = scan_new.next();
+                //newfile.newfile_path="/Users/aa404863/Documents/JavaProj/text_new.txt";
+                newfile.WriteFile(new_list);
+            }else{
+                Roleback_File newfile = new Roleback_File();
+                newfile.PrintFile(new_list);
+            }
+        }catch (FileNotFoundException e) {
+        System.out.print("Please check this file path one more time");
+        }
+    }
+}
 
-public class ReadFileAndWrite{
-    public static void main(String[] args)throws IOException{
-        //1.每一行的内容读取出来存在list<String>集合
-        //1.1 集合对象
+class This_File{
+    //1.read and save in list<String> 
+    String file_path;
+    public List<String> ReadFile() throws IOException{ 
+
         List<String> list = new ArrayList<String>();
-        //1.2一行行读取文件
-        BufferedReader br = new BufferedReader(new FileReader("/Users/aa404863/Desktop/text.txt"));
-        String line =null;
-        while((line=br.readLine())!=null){//读取一个字符串
+        BufferedReader br = new BufferedReader(new FileReader(this.file_path));
+        String line = null;
+        while((line = br.readLine()) != null){//read the String by Line）
             list.add(line);
         }
-        //2.当我们要写入文件时，倒序保存list集合中的内容到文件中
-        BufferedWriter bw = new BufferedWriter(new FileWriter("/Users/aa404863/Desktop/text_new.txt"));
-        for(int i= list.size()-1;i>=0;i--){
-            bw.write(list.get(i));
-            bw.newLine();//换行
-            System.out.println(list.get(i));
-            //System.out.println(“\r\n”);
-            
-        }
-        br.close();
-        bw.close();
-       //System.out.println("finished");
+        br.close();//close the br
+        return list;
         
     }
+
+}
+
+class Roleback_File{
+    String newfile_path;
+    public void WriteFile(List<String> new_list) throws IOException{
+        BufferedWriter bw = new BufferedWriter(new FileWriter(this.newfile_path));
+        for(int i = new_list.size()-1;i>=0;i--){
+            bw.write(new_list.get(i));
+            bw.newLine();
+            System.out.println(new_list.get(i));
+            //System.out.println(“\r\n”); 
+        }
+        bw.close();//close the bw
+    }
+    public void PrintFile(List<String> new_list)throws IOException {
+        for(int i = new_list.size()-1;i>=0;i--){
+            System.out.println(new_list.get(i));
+        }
+    }
+
 }
---
+++ Readme.txt
@@ -0,0 +1,35 @@
+ReadMe：
+Function: This program is to read the file content and output the file content in reverse by line.
+
+java version: java 13.0.2
+
+Supported file types: .txt
+
+Supported operating environment: macOS, program can be operated by command-line
+
+Compile and run method:
+
+1.Please write the path of your file(Input PATH) that you want to read  in  ReadFileAndWrite.java
+
+
+  You also need to write the path of directory(Output PATH)that you want to output newfile
+
+1. Open your terminal, and  come to the directory where has ReadFileAndWrite.jav, paste this path and 
+   
+   write in terminal like this: cd path(eg. cd /Users/aa404863/Documents/JavaProj)
+
+2. Please write: javac -d . ReadFileAndWrite.jav   (here,you can see new directory has new .class files)
+
+3. Please write: java tsdl.training.ReadFileAndWrite . And enter,you can see the new result!
+
+4. When terminal says "please enter your file path",you can write the path of your file that you want to read
+
+5. Then terminal will say "Do you want to save new content in new file? Yes or No".
+
+   If you want to save new content in new file, please write "Yes",and then write the path of newfile.
+ 
+   If your answer is "No",you can only see the reverse content.
+ 
+   
+
+
